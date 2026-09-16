fc50008e7dd55d2b6f7d5f9366307a018fe027c1
DaggBoot <148903817+DaggBoot@users.noreply.github.com>
2026-09-16 14:24:06 -0400

    EX06 completion

--- code/src/main/java/cs/toronto/edu/csc207/hello/HelloWorld.java
+++ code/src/main/java/cs/toronto/edu/csc207/hello/HelloWorld.java
@@ -1,14 +1,23 @@
 package cs.toronto.edu.csc207.hello;
 
-/** Introductory Hello World program demonstrating the basics of Java. */
-public class HelloWorld {
 
-  /**
-   * Entry point — prints a greeting to standard output.
-   *
-   * @param args command-line arguments (unused).
-   */
+///** Introductory Hello World program demonstrating the basics of Java. */
+//public class HelloWorld {
+//
+//  /**
+//   * Entry point — prints a greeting to standard output.
+//   *
+//   * @param args command-line arguments (unused).
+//   */
+//  public static void main(String[] args) {
+//    System.out.println("Hello, World!");
+//  }
+//}
+
+public class HelloWorld{
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+      int a = 1;
+      System.out.printf("Hello my number %s", a);
+
   }
 }
