e815e26a9a67b1e35da5f369423a5868e9057730
DaggBoot <148903817+DaggBoot@users.noreply.github.com>
2026-09-17 23:01:24 -0400

    EX04 completion

--- exercises/ex04-digit-sum/src/main/java/DigitSum.java
+++ exercises/ex04-digit-sum/src/main/java/DigitSum.java
@@ -21,7 +21,14 @@ public class DigitSum {
      * @return the sum of its decimal digits
      */
     public static int digitSum(int n) {
-        // TODO: complete
-        return 0;
+        int num = Math.abs(n);
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+
+        return sum;
     }
 }
